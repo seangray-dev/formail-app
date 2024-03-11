335a2099402ac0005922473623a722b87dd0c378
Sean Gray <seangray.dev@gmail.com>
2024-03-11 02:48:57 -0400

    Refactor schema.ts: Update forms and users table structure

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,23 +1,21 @@
 import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
-export const roles = v.union(v.literal('admin'), v.literal('member'));
-
 export default defineSchema({
   forms: defineTable({
+    description: v.string(),
     name: v.string(),
     orgId: v.string(),
-    description: v.string(),
   }).index('by_orgId', ['orgId']),
   users: defineTable({
-    tokenIdentifier: v.string(),
-    name: v.optional(v.string()),
     image: v.optional(v.string()),
+    name: v.optional(v.string()),
     orgIds: v.array(
       v.object({
         orgId: v.string(),
-        role: roles,
+        role: v.union(v.literal('admin'), v.literal('member')),
       })
     ),
+    tokenIdentifier: v.string(),
   }).index('by_tokenIdentifier', ['tokenIdentifier']),
 });
