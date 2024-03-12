a0211e4124950061d70aee165577b23fbccf72e1
Sean Gray <seangray.dev@gmail.com>
2024-03-12 07:44:16 -0400

    Add copy to clipboard functionality for form ID

--- src/components/layout/DashboardHeader/dash-board-header.tsx
+++ src/components/layout/DashboardHeader/dash-board-header.tsx
@@ -8,14 +8,18 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
 import { useOrganization, useUser } from '@clerk/nextjs';
 import { useQuery } from 'convex/react';
+import { ClipboardCheckIcon, ClipboardIcon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import { api } from '../../../../convex/_generated/api';
 import { Id } from '../../../../convex/_generated/dataModel';
 
 export default function DashboardHeader() {
+  const [isCopied, setIsCopied] = useState(false);
   const pathname = usePathname();
   const organization = useOrganization();
   const user = useUser();
@@ -40,8 +44,18 @@ export default function DashboardHeader() {
   const formQueryArg =
     user.isLoaded && formId ? { formId: formId as Id<'forms'> } : 'skip';
   const form = useQuery(api.forms.getFormById, formQueryArg);
-
   let formName = form?.name ?? 'Unknown';
+
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    } catch (error) {
+      console.error('failed to copy', error);
+    }
+  };
+
   return (
     <>
       <Breadcrumb>
@@ -79,8 +93,27 @@ export default function DashboardHeader() {
             <p>
               Form Name: <span className='text-white'>{formName}</span>
             </p>
-            <p>
+            <p className='flex gap-2 items-center'>
               Form ID: <span className='text-white'>{form?._id}</span>
+              <Button
+                title='Copy to clipboard'
+                size={'icon'}
+                variant={'ghost'}
+                className='p-0 m-0 w-fit h-fit hover:bg-transparent mb-1 relative'
+                onClick={() => {
+                  if (form?._id) {
+                    copyToClipboard(form._id);
+                  }
+                }}>
+                {isCopied ? (
+                  <ClipboardCheckIcon size={20} />
+                ) : (
+                  <ClipboardIcon size={20} />
+                )}
+                {isCopied && (
+                  <div className='absolute bottom-6 text-xs'>Copied!</div>
+                )}
+              </Button>
             </p>
           </div>
           <nav className='mt-6 border-b pb-2 flex gap-4 text-muted-foreground'>
