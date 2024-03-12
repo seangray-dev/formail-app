ede9ea51db8d233537e601994153494e60e3f7c9
Sean Gray <seangray.dev@gmail.com>
2024-03-12 03:37:09 -0400

    test: Add CORS headers for form submissions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // route for form submissions
+        source: '/submit/:formId',
+        headers: [
+          // Allow requests from any origin
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          // Specify the allowed HTTP methods
+          { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' },
+          // Specify the allowed headers
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+          // Optional: Allow credentials
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
