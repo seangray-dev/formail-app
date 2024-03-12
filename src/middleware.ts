3a17316c102186efcbcd7e3e6a8bb8099a5b1ebe
Sean Gray <seangray.dev@gmail.com>
2024-03-12 04:56:16 -0400

    Add '/contact' and '/documentation' to publicRoutes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/submit/(.*)'],
+  publicRoutes: ['/', '/submit/(.*)', '/contact', '/documentation'],
   // Routes that can always be accessed, and have
   // no authentication information
 });
