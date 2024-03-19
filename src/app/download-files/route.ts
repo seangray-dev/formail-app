8c5e8998b0a4b515b47de9e7cd898ec03702a1bb
Sean Gray <seangray.dev@gmail.com>
2024-03-19 13:34:24 -0400

    in prog... download files functionality

---
+++ src/app/download-files/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  // const searchParams = request.nextUrl.searchParams;
+  // const fileId = searchParams.get('fileId');
+  // console.log(fileId);
+  // // Replace this with your actual logic to retrieve the file based on fileId
+  // const fileData = await getFileData(fileId);
+  // const headers = new Headers({
+  //   'Content-Type': 'application/octet-stream', // or the actual content type of your file
+  //   'Content-Disposition': `attachment; filename="${fileData.fileName}"`,
+  // });
+  // return new Response(fileData.content, { headers });
+}
