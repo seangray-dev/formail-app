ff319fab1028de0d65936ed975b1ca1bf25d7c33
Sean Gray <seangray.dev@gmail.com>
2024-03-19 09:33:49 -0400

    add sorting by newst/oldest functionality

--- src/app/dashboard/[orgId]/form/[formId]/files/page.tsx
+++ src/app/dashboard/[orgId]/form/[formId]/files/page.tsx
@@ -32,16 +32,22 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { useToast } from '@/components/ui/use-toast';
 import { formDetailsAtom } from '@/jotai/state';
 import { useMutation, useQuery } from 'convex/react';
 import { formatRelative } from 'date-fns';
 import { useAtom } from 'jotai';
-import { FilesIcon } from 'lucide-react';
+import { EyeIcon, FilesIcon, TrashIcon } from 'lucide-react';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { api } from '../../../../../../../convex/_generated/api';
 import { Id } from '../../../../../../../convex/_generated/dataModel';
 type submissionId = Id<'submissions'>;
@@ -59,6 +65,7 @@ export default function SubmissionsPage() {
   const [selectedSubmissionId, setSelectedSubmissionId] =
     useState<Id<'submissions'> | null>(null);
   const [isDetailsDialogOpen, setIsDetailsDialogOpen] = useState(false);
+  const [sortOrder, setSortOrder] = useState('newest');
 
   const formQueryArg = formId ? { formId: formId as Id<'forms'> } : 'skip';
   const submissions = useQuery(
@@ -98,17 +105,23 @@ export default function SubmissionsPage() {
   );
 
   const handleDownloadAllFiles = () => {
-    selectedSubmission?.files?.forEach((file, index) => {
-      setTimeout(() => {
-        const link = document.createElement('a');
-        link.href = file.url;
-        link.download = `File_${index + 1}`; // You might want to use a more descriptive name
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }, index * 100); // 100ms delay between each download
+    console.log('Downloading all files');
+    console.log('Selected Submission', selectedSubmission);
+    selectedSubmission?.files?.forEach((file) => {
+      window.open(`/download-files?fileId=${file.storageId}`, '_blank');
     });
   };
+
+  const handleSortChange = (value: string) => {
+    setSortOrder(value);
+  };
+
+  const sortedSubmissions = [...(submissions || [])].sort((a, b) => {
+    const dateA = new Date(a._creationTime).getTime();
+    const dateB = new Date(b._creationTime).getTime();
+
+    return sortOrder === 'newest' ? dateB - dateA : dateA - dateB;
+  });
 
   function RenderFile({
     file,
@@ -135,7 +148,7 @@ export default function SubmissionsPage() {
           <iframe
             src={file.url}
             title={`PDF File ${index + 1}`}
-            width='500'
+            width='400'
             height='600'></iframe>
         );
       default:
@@ -150,30 +163,49 @@ export default function SubmissionsPage() {
 
   return (
     <section className='container flex-1 flex flex-col mx-auto p-4'>
-      <h3 className='text-lg font-semibold mb-4'>Files</h3>
-      {submissions && submissions.length > 0 ? (
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-          {submissions.map((submission) => (
+      <div className='self-end mb-10 flex gap-4'>
+        <Button className='max-w-44'>Download All Files</Button>
+        <Select
+          onValueChange={(value) => {
+            handleSortChange(value);
+          }}>
+          <SelectTrigger className='w-44'>
+            <SelectValue placeholder='Sort' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='newest'>Newest</SelectItem>
+            <SelectItem value='oldest'>Oldest</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      {sortedSubmissions && sortedSubmissions.length > 0 ? (
+        <div className='flex flex-col sm:flex-row flex-wrap gap-4 justify-between'>
+          {sortedSubmissions.map((submission) => (
             <Card
               key={submission._id}
-              className='max-w-xs flex flex-col gap-4 items-center p-4'>
-              <CardHeader>
-                <FilesIcon size={48} />
+              className='sm:w-80 flex flex-col items-center p-4'>
+              <CardHeader className='p-0 mb-6'>
+                <CardTitle className='text-xs'>ID: {submission._id}</CardTitle>
               </CardHeader>
-              <CardContent>
-                <p>{formatRelative(submission._creationTime, new Date())}</p>
+              <CardContent className='flex flex-col items-center p-0 mb-6'>
+                <FilesIcon className='w-20 h-20 mb-4' />
+                <p className='text-sm text-muted-foreground'>
+                  {formatRelative(submission._creationTime, new Date())}
+                </p>
               </CardContent>
-              <CardFooter className='p-0 flex flex-col gap-2 w-full'>
+              <CardFooter className='p-0 flex flex-col md:flew-row gap-2 w-full'>
                 <Button
                   variant='secondary'
-                  className='w-full'
+                  className='w-full flex items-center gap-2'
                   onClick={() => handleDetailsClick(submission._id)}>
+                  <EyeIcon size={18} />
                   View Details
                 </Button>
                 <Button
                   variant='destructive'
-                  className='w-full'
+                  className='w-full flex items-center gap-2'
                   onClick={() => handleDeleteClick(submission._id)}>
+                  <TrashIcon size={18} />
                   Delete
                 </Button>
               </CardFooter>
@@ -188,18 +220,17 @@ export default function SubmissionsPage() {
           </p>
         </div>
       )}
-
       <AlertDialog
         open={isDeleteDialogOpen}
         onOpenChange={setIsDeleteDialogOpen}>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>
-              Are you sure you want to delete this submission?
+              Are you sure you want to delete this submission & file{'(s)'}?
             </AlertDialogTitle>
             <AlertDialogDescription>
               This action cannot be undone. This will permanently delete this
-              submission and remove it from our servers.
+              submission and any associated file{'(s)'} from our servers.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
@@ -212,9 +243,8 @@ export default function SubmissionsPage() {
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-
       <Dialog open={isDetailsDialogOpen} onOpenChange={setIsDetailsDialogOpen}>
-        <DialogContent>
+        <DialogContent className='px-10'>
           <DialogHeader>
             <DialogTitle>File Details</DialogTitle>
           </DialogHeader>
@@ -239,7 +269,7 @@ export default function SubmissionsPage() {
               <Carousel>
                 <CarouselContent>
                   {selectedSubmission.files.map((file, index) => (
-                    <CarouselItem className='px-10'>
+                    <CarouselItem className='flex flex-col justify-center items-center'>
                       <RenderFile
                         file={file as FileWithUrl}
                         index={index ?? 0}
@@ -247,17 +277,12 @@ export default function SubmissionsPage() {
                     </CarouselItem>
                   ))}
                 </CarouselContent>
-                <CarouselPrevious className='ml-8' />
-                <CarouselNext className='mr-8' />
+                <CarouselPrevious className='ml-3' />
+                <CarouselNext className='mr-3' />
               </Carousel>
               <DialogFooter className='mx-auto'>
                 <div className='mt-4 text-center'>
-                  <Button
-                    onClick={() => {
-                      selectedSubmission.files.forEach((file) => {
-                        window.open(file.url, '_blank');
-                      });
-                    }}>
+                  <Button onClick={handleDownloadAllFiles}>
                     Download File{'(s)'}
                   </Button>
                 </div>
