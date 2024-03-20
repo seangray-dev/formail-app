44ab13cf14ad67c93799b287895144d283d265b6
Sean Gray <seangray.dev@gmail.com>
2024-03-20 02:39:48 -0400

    Add export functionality for JSON and CSV files

--- src/app/dashboard/[orgId]/form/[formId]/submissions/page.tsx
+++ src/app/dashboard/[orgId]/form/[formId]/submissions/page.tsx
@@ -51,6 +51,9 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 import { api } from '../../../../../../../convex/_generated/api';
 import { Id } from '../../../../../../../convex/_generated/dataModel';
 type submissionId = Id<'submissions'>;
+type SubmissionData = {
+  [key: string]: string;
+};
 
 export default function SubmissionsPage() {
   const { toast } = useToast();
@@ -120,6 +123,64 @@ export default function SubmissionsPage() {
     }
   };
 
+  const selectedData: SubmissionData[] =
+    submissions
+      ?.filter((submission) => checkedSubmissions.has(submission._id))
+      .map((submission) => JSON.parse(submission.data)) || [];
+
+  const exportToJson = (selectedData: SubmissionData[]) => {
+    if (!selectedData) return;
+
+    const dataStr = JSON.stringify(selectedData);
+    const dataUri =
+      'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+    const exportFileDefaultName = 'data.json';
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+  };
+
+  const convertToCSV = (objArray: SubmissionData[]) => {
+    if (!objArray || objArray.length === 0) return '';
+
+    // Collect all unique headers/keys from all objects
+    const allHeaders = new Set<string>();
+    objArray.forEach((obj) =>
+      Object.keys(obj).forEach((key) => allHeaders.add(key))
+    );
+
+    // Convert Set of headers to Array
+    const headers = Array.from(allHeaders);
+
+    // Construct CSV string
+    const csvRows = [
+      // Headers row
+      headers.join(','),
+      ...objArray.map((row) =>
+        // Map each value under its header
+        headers.map((header) => JSON.stringify(row[header] || '')).join(',')
+      ),
+    ];
+
+    // Join all rows with new line
+    return csvRows.join('\r\n');
+  };
+
+  const exportToCsv = (selectedData: SubmissionData[]) => {
+    // Ensure there is data to export
+    if (!selectedData || selectedData.length === 0) return;
+
+    const csvStr = convertToCSV(selectedData);
+    const dataUri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvStr);
+    const exportFileDefaultName = 'data.csv';
+
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+  };
+
   return (
     <section className='container flex-1 flex flex-col'>
       {submissions && submissions.length > 0 ? (
@@ -138,8 +199,12 @@ export default function SubmissionsPage() {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                  <DropdownMenuItem>JSON</DropdownMenuItem>
-                  <DropdownMenuItem>CSV</DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => exportToJson(selectedData)}>
+                    JSON
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => exportToCsv(selectedData)}>
+                    CSV
+                  </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
               <Button
