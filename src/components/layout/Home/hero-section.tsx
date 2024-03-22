f0634df0afcc758ea34561d9c79f90fc97e8e3a2
Sean Gray <seangray.dev@gmail.com>
2024-03-22 01:03:52 -0400

    Refactor hero section and update copy

--- src/components/layout/Home/hero-section.tsx
+++ src/components/layout/Home/hero-section.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { CheckIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+
+const words = ['Feedback', 'Contact Form', 'Survey', 'Submission'];
 
 export default function HeroSection() {
   return (
@@ -10,21 +14,29 @@ export default function HeroSection() {
       <div>
         <div className='flex flex-col gap-3 md:gap-4 2xl:gap-5 max-w-xl text-center md:text-left'>
           <h1 className='font-extrabold tracking-tight leading-none text-3xl md:text-5xl 2xl:text-6xl capitalize'>
-            Streamline your workflow with Formail
+            Make{' '}
+            <span
+              className={`bg-clip-text bg-gradient-to-r from-zinc-300 to-zinc-600 text-transparent transition-opacity`}>
+              Form
+            </span>{' '}
+            Management Effortless with Formail
           </h1>
           <p className='text-muted-foreground'>
-            Formail simplifies your workflow by revolutionizing form submission
-            management, turning each entry into a step towards your goals. Our
-            platform not just collects data but unlocks smarter decisions.
+            Streamline your form handling effortlessly with our intuitive
+            platform. Assign unique IDs to your forms, utilize our API for
+            seamless submissions, and enjoy tailored management solutions
+            perfect for developers and small businesses. Ideal for contact
+            forms, surveys, and feedback, Formail enhances your strategy
+            seamlessly.
           </p>
         </div>
-        <div className='mt-12 flex flex-col md:flex-row gap-3 items-center justify-start md:items-center'>
-          <Button asChild className='max-w-56'>
+        <div className='mt-12 flex flex-col 2xl:flex-row gap-3 2xl:items-center justify-start'>
+          <Button asChild className='max-w-52'>
             <Link href={'/sign-up'}>Get Started For Free</Link>
           </Button>
-          <div className='flex gap-1'>
-            <CheckIcon className='text-muted-foreground' />
-            <p>No Credit Card Required</p>
+          <div className='flex items-center gap-1'>
+            <CheckIcon size={18} className='text-muted-foreground' />
+            <p className='text-sm'>No Credit Card Required</p>
           </div>
         </div>
       </div>
