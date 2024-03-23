05c16c6e063c3100f67685dde3cb404b6cea8505
Sean Gray <seangray.dev@gmail.com>
2024-03-23 18:40:01 -0400

    Update authMiddleware routes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,16 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/submit/(.*)', '/contact', '/documentation'],
+  publicRoutes: ['/', '/submit/(.*)'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: [
+    '/contact',
+    '/documentation',
     '/legal/cookie-policy',
     '/legal/privacy-policy',
     '/legal/terms-&-conditions',
+    '/pricing',
   ],
 });
 
