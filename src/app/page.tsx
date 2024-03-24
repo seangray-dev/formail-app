cede538a7e09c154701d6e6a171f71fbdb8c5faf
Sean Gray <seangray.dev@gmail.com>
2024-03-23 23:17:41 -0400

    Update page and hero section styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from '@/components/layout/Home/hero-section';
 
 export default function Home() {
   return (
-    <main className='container flex-1'>
+    <main className='container flex-1 my-8 md:my-20'>
       <HeroSection />
       <FeaturesSection />
     </main>
--- src/components/layout/Home/hero-section.tsx
+++ src/components/layout/Home/hero-section.tsx
@@ -6,11 +6,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const words = ['Feedback', 'Contact Form', 'Survey', 'Submission'];
-
 export default function HeroSection() {
   return (
-    <section className='flex flex-col-reverse gap-16 justify-between items-center md:grid md:grid-cols-2 mt-8 md:mt-20 md:gap-10 mb-20 md:mb-32 2xl:mb-36'>
+    <section className='flex flex-col-reverse gap-16 justify-between items-center md:grid md:grid-cols-2 md:gap-10 mb-20 md:mb-32 2xl:mb-36'>
       <div>
         <div className='flex flex-col gap-3 md:gap-4 2xl:gap-5 max-w-xl text-center md:text-left'>
           <h1 className='font-extrabold tracking-tight leading-none text-3xl md:text-5xl 2xl:text-6xl capitalize'>
@@ -19,7 +17,7 @@ export default function HeroSection() {
               className={`bg-clip-text bg-gradient-to-r from-zinc-300 to-zinc-600 text-transparent transition-opacity`}>
               Form
             </span>{' '}
-            Management Effortless with Formail
+            Management Effortless
           </h1>
           <p className='text-muted-foreground'>
             Streamline your form handling effortlessly with our intuitive
@@ -30,7 +28,7 @@ export default function HeroSection() {
             seamlessly.
           </p>
         </div>
-        <div className='mt-12 flex flex-col 2xl:flex-row gap-3 2xl:items-center justify-start'>
+        <div className='mt-12 flex flex-col gap-3 md:items-start justify-start items-center'>
           <Button asChild className='max-w-52'>
             <Link href={'/sign-up'}>Get Started For Free</Link>
           </Button>
