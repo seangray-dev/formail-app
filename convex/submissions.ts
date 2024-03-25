7ee2ca2b6aba9c4c62fcc5b8f08ab0dbe7496458
Sean Gray <seangray.dev@gmail.com>
2024-03-25 10:21:05 -0400

    readd deleteSubmissionById

--- convex/submissions.ts
+++ convex/submissions.ts
@@ -106,6 +106,43 @@ export const getSubmissionsByFormId = query({
   },
 });
 
+export const deleteSubmissionById = mutation({
+  args: { submissionId: v.id('submissions') },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError('No user identity provided');
+    }
+
+    // Retrieve the submission
+    const submission = await ctx.db.get(args.submissionId);
+    if (!submission) {
+      throw new ConvexError('This submission does not exist');
+    }
+
+    // Retrieve the form associated with the submission to get the orgId
+    const form = await ctx.db.get(submission.formId);
+    if (!form) {
+      throw new ConvexError(
+        'Form associated with this submission does not exist'
+      );
+    }
+
+    // Check if the user has access to the org and is an admin
+    const hasAccess = await isAdminOfOrg(ctx, form.orgId);
+
+    if (!hasAccess) {
+      throw new ConvexError(
+        'You do not have permission to delete this submission'
+      );
+    }
+
+    // Proceed with deletion
+    await ctx.db.delete(args.submissionId);
+  },
+});
+
 export const getSubmissionsByDateRange = query({
   args: {
     formId: v.id('forms'),
