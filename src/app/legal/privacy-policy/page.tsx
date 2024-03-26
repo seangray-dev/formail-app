dd10b9d508c7e6c437ae07d9e1985daee8ca6e6f
Sean Gray <seangray.dev@gmail.com>
2024-03-25 20:57:15 -0400

    add privacy policy content

--- src/app/legal/privacy-policy/page.tsx
+++ src/app/legal/privacy-policy/page.tsx
@@ -1,5 +1,329 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 export default function PrivacyPolicyPage() {
-  return <div className='container flex-1'>PrivacyPolicyPage</div>;
+  return (
+    <section className="prose prose-invert">
+      <div>
+        <h1>Formail Privacy Policy</h1>
+        <p>
+          Our commitment is to the protection of your data and private
+          information.
+        </p>
+        <p>
+          Formail operates with privacy as a priority. Our revenue comes from
+          our customers, not from selling your data. We have never and will
+          never sell your data.
+        </p>
+        <p>
+          This policy outlines: the data we collect and the reasons for
+          collecting it; how we handle your data; and your rights regarding your
+          data.
+        </p>
+        <p>
+          By using Formail ("Service"), you are consenting to the data practices
+          described in this policy.
+        </p>
+      </div>
+
+      <div>
+        <h2>Data Collection and Purpose</h2>
+        <h3>Personal Information</h3>
+        <p>
+          During your use of the Service, we generally request the following
+          personal information:
+        </p>
+        <ul>
+          <li>Email address</li>
+          <li>First and Last name</li>
+          <li>Phone number</li>
+          <li>Country, State, Province, ZIP/Postal code, City</li>
+        </ul>
+        <p>The collected information is utilized in the following manners:</p>
+        <ul>
+          <li>
+            To tailor your experience and enable us to provide content and
+            product offerings that align with your interests.
+          </li>
+          <li>To enhance our website and services.</li>
+          <li>To improve our customer service response.</li>
+          <li>
+            To manage a contest, promotion, survey, or other website features.
+          </li>
+          <li>To solicit ratings and reviews of services or products.</li>
+          <li>
+            To follow up after interactions (live chat, email, or phone
+            inquiries).
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Payment Information</h2>
+        <ul>
+          <li>
+            All credit card transactions are managed by external PCI-compliant
+            payment gateways (such as "Stripe") and are neither stored nor
+            processed on our servers.
+          </li>
+          <li>
+            We maintain a record of each payment transaction, which includes the
+            last 4 digits of the credit card number and the billing address at
+            the time, for the purposes of account history, invoicing, and
+            billing assistance.
+          </li>
+          <li>
+            Your billing address is stored to calculate any applicable sales tax
+            in the United States or VAT in the EU, to identify potential
+            fraudulent credit card transactions, and to include on your
+            invoices.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Geolocation Information</h2>
+        <p>We gather and geocode IP addresses for the following reasons:</p>
+        <ul>
+          <li>
+            To identify and limit traffic from certain geographical areas.
+          </li>
+          <li>
+            To identify and limit traffic from specific IP addresses and address
+            ranges.
+          </li>
+          <li>
+            To identify and limit traffic that is considered harmful for any
+            reason.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Interactions with Website and Server</h2>
+        <p>
+          To ensure service quality and generate overall website statistics, we
+          gather information about your computer&apos;s hardware and software.
+        </p>
+        <p>The collected information may include, but is not limited to:</p>
+        <ul>
+          <li>IP address</li>
+          <li>Operating System</li>
+          <li>Times of access</li>
+          <li>Pages visited</li>
+          <li>Page load times</li>
+          <li>Language settings</li>
+          <li>Referring website URLs</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Cookies</h2>
+        <p>
+          We utilize cookies to save specific preferences, simplify your
+          interaction with our applications, and aid in analytics (such as
+          identifying and tracking visitors).
+        </p>
+        <p>
+          A cookie is a text fragment that your browser stores to assist in
+          remembering your login details, site preferences, and more.
+        </p>
+        <p>
+          You have the ability to modify the cookie retention settings in your
+          browser.
+        </p>
+        <p>
+          For more information about cookies, including how to see which cookies
+          have been set and how to manage and delete them, please visit:
+          www.allaboutcookies.org.
+        </p>
+      </div>
+
+      <div>
+        <h2>Voluntary Correspondence</h2>
+        <p>
+          When you contact us with a question or request for assistance, we
+          retain that correspondence, including the email address, to maintain a
+          history of past interactions for future reference.
+        </p>
+        <p>
+          We also keep any information you voluntarily provide, such as survey
+          responses.
+        </p>
+      </div>
+
+      <div>
+        <h2>Data We Don't Collect</h2>
+        <p>
+          We refrain from collecting any attributes of protected classifications
+          such as age, race, gender, religion, sexual orientation, gender
+          identity, gender expression, as well as physical and mental abilities
+          or disabilities. Additionally, we do not collect any biometric data.
+        </p>
+      </div>
+
+      <div>
+        <h2>Data Storage Method</h2>
+        <p>
+          We rely on third-party vendors and hosting partners for the essential
+          hardware, software, networking, storage, and related technology needed
+          to operate the Service.
+        </p>
+      </div>
+
+      <div>
+        <h2>Where we store your datan</h2>
+        <p>
+          Our operation is based in Canada, but due to the global nature of our
+          infrastructure providers, your data may be stored and processed in
+          multiple data centers around the world.
+        </p>
+        <p>
+          By using our service, you acknowledge that your information may be
+          transferred to, stored in, and processed in various international
+          locations by our trusted service partners as part of providing you
+          with a high-quality and secure service.
+        </p>
+      </div>
+
+      <div>
+        <h2>Data Security Measures</h2>
+        <p>
+          Your data is safeguarded behind secure networks and is only accessible
+          to a select number of individuals with special access rights to these
+          systems, who are obligated to maintain the confidentiality of the
+          information. Furthermore, any sensitive information you provide is
+          encrypted using Secure Socket Layer (SSL) technology.
+        </p>
+        <p>
+          Personal data is stored in a format that allows identification only
+          for the duration necessary for the processing purposes.
+        </p>
+        <p>
+          All credit card transactions are handled by external PCI-compliant
+          payment gateways ("Stripe") and are not stored or processed on our
+          servers.
+        </p>
+        <p>
+          Despite our efforts to protect your data using commercially acceptable
+          means, we cannot assure its absolute security.
+        </p>
+      </div>
+
+      <div>
+        <h2>Ownership of Your Data</h2>
+        <p>
+          Despite the Service's ownership of the code, databases, and all rights
+          to the application of the Service, you maintain all rights to your
+          data.
+        </p>
+      </div>
+
+      <div>
+        <h2>Information Fairness Practices</h2>
+        <p>
+          In the event of a data breach, we will send you a notification via
+          email within 7 business days
+        </p>
+      </div>
+
+      <div>
+        <h2>Disclosure to Third Parties</h2>
+        <p>
+          We do not trade your personally identifiable information with third
+          parties.
+        </p>
+        <p>
+          We may engage reputable third-party companies and individuals to
+          facilitate the Service, deliver the Service on our behalf, carry out
+          Service-related tasks, or assist us in understanding how the Service
+          is utilized.
+        </p>
+        <p>
+          These third parties have access to your personally identifiable
+          information solely to accomplish these tasks on our behalf and are
+          committed not to disclose or use it for any other purpose.
+        </p>
+        <p>Please refer to this page for a list of our subprocessors.</p>
+      </div>
+
+      <div>
+        <h2>Links to Third-Party Sites</h2>
+        <p>
+          The Service may include links to external sites not operated by us. If
+          you click on a third-party link, you will be redirected to that third
+          party's site.
+        </p>
+        <p>
+          We strongly recommend that you review the Privacy Policy of every site
+          you visit.
+        </p>
+        <p>
+          We do not have control over and do not assume any responsibility for
+          the content, privacy policies, or practices of any third-party site or
+          service.
+        </p>
+      </div>
+
+      <div>
+        <h2>Policy on Children&apos;s Privacy</h2>
+        <p>
+          Our platform is not designed to attract or engage children under the
+          age of 13. We are committed to protecting the privacy of young users
+          and do not knowingly collect personal information from children under
+          this age.
+        </p>
+        <p>
+          We encourage parents and guardians to take an active role in their
+          children&apos;s online activities. If you believe your child has
+          provided us with their personal information without your consent,
+          please reach out to us immediately.
+        </p>
+        <p>
+          In the event we discover that personal information from children under
+          the age of 13 has been inadvertently collected without the requisite
+          consent from their guardians, we will promptly undertake measures to
+          eliminate such data from our records.
+        </p>
+      </div>
+
+      <div>
+        <h2>Policy Updates</h2>
+        <p>
+          We will revise this policy to ensure compliance with applicable
+          regulations and to incorporate company and customer feedback.
+        </p>
+        <p>
+          We recommend that you review this policy periodically to stay informed
+          about how we safeguard your information.
+        </p>
+        <p>
+          Whenever we make a significant modification to this policy, we will
+          notify you via email and/or a prominent notice on the Service.
+        </p>
+        <p>
+          Changes to this policy become effective when they are published on
+          this page.
+        </p>
+      </div>
+
+      <div>
+        <h2>Inquiries</h2>
+        <p>
+          Do you have any questions, comments, or concerns about this policy,
+          your data, or your rights regarding your information?
+        </p>
+        <p>
+          Please reach out to us by contacting us{" "}
+          <Link href={"/contact"}>here</Link>.
+        </p>
+        <p>You can also reach out to us at any time to:</p>
+        <ul>
+          <li>Request access to the information we hold about you.</li>
+          <li>Amend any information we hold about you.</li>
+          <li>Erase any information we hold about you.</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
