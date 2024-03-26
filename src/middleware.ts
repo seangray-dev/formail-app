86cf6013dbb067fe2ef4b08db7d57009d7fef12e
Sean Gray <seangray.dev@gmail.com>
2024-03-26 14:33:46 -0400

    Update middleware routes in src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,19 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/submit/(.*)'],
+  publicRoutes: ["/", "/submit/(.*)"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: [
-    '/contact',
-    '/documentation',
-    '/legal/cookie-policy',
-    '/legal/privacy-policy',
-    '/legal/terms-&-conditions',
-    '/pricing',
+    "/docs/(.*)",
+    "/docs",
+    "/contact",
+    "/documentation",
+    "/legal/cookie-policy",
+    "/legal/privacy-policy",
+    "/legal/terms-&-conditions",
+    "/pricing",
   ],
 });
 
@@ -19,5 +21,5 @@ export const config = {
   // Protects all routes, including api/trpc.
   // See https://clerk.com/docs/references/nextjs/auth-middleware
   // for more information about configuring your Middleware
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
