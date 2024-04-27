0e0b67b428439cdd4fe9f157e67914f5469d919b
Sean Gray <seangray.dev@gmail.com>
2024-04-27 01:14:58 -0400

    fix padding on cta-section

--- src/components/layout/Home/cta-section.tsx
+++ src/components/layout/Home/cta-section.tsx
@@ -2,7 +2,7 @@ import CTAButton from "./cta-button";
 
 export default function CTASection() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 bg-secondary py-1">
+    <section className="flex flex-col items-center justify-center gap-4 bg-secondary py-12">
       <h6 className="text-lg">Get Started for free</h6>
       <CTAButton />
     </section>
