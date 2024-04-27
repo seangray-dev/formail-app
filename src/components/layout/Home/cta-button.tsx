2741be8320bfe6d35ef66693393cdd7b62d1c0fd
Sean Gray <seangray.dev@gmail.com>
2024-04-27 05:49:48 -0400

    Add posthog event tracking to CTA button

--- src/components/layout/Home/cta-button.tsx
+++ src/components/layout/Home/cta-button.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { usePostHog } from "posthog-js/react";
 import React from "react";
 
 export default function CTAButton() {
+  const posthog = usePostHog();
+
   return (
-    <Button asChild>
+    <Button
+      asChild
+      onClick={() => {
+        posthog.capture("cta button clicked");
+      }}
+    >
       <Link href={"/sign-up"}>Manage Your Forms For Free</Link>
     </Button>
   );
