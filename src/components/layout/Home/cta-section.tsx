e588f1f97696d9f24c48dd32bee06703b3f6ea5f
Sean Gray <seangray.dev@gmail.com>
2024-04-27 15:29:59 -0400

    add background-boxes component and apply to cta-section

--- src/components/layout/Home/cta-section.tsx
+++ src/components/layout/Home/cta-section.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { Boxes } from "@/components/ui/background-boxes";
+import React from "react";
 import CTAButton from "./cta-button";
 
 export default function CTASection() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 bg-secondary py-12">
-      <h6 className="text-lg">Get Started for free</h6>
-      <CTAButton className="bg-background" />
+    <section className="relative flex h-96 w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-secondary">
+      <div className="pointer-events-none absolute inset-0 z-20 h-full w-full bg-secondary [mask-image:radial-gradient(transparent,white)]" />
+      <Boxes />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h6 className="relative z-20 text-xl text-white md:text-4xl">
+          Get Started For Free
+        </h6>
+        <CTAButton className="bg-background" />
+      </div>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -74,7 +80,22 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    addVariablesForColors,
+  ],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
