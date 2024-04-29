f5f3875397a5a95cc1082778bac11d9dc99b2510
Sean Gray <seangray.dev@gmail.com>
2024-04-29 05:17:53 -0400

    Fix eslint build error in not-found.tsx file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,9 @@ export default function NotFound() {
       <Image src={"/not_found.svg"} width={200} height={200} alt="" />
       <div className="mt-12 flex flex-col gap-2 text-center">
         <h1 className="text-3xl font-bold">404</h1>
-        <p>Oops! We can't seem to find the page you're looking for.</p>
+        <p>
+          Oops! We can&apos;t seem to find the page you&apos;re looking for.
+        </p>
         <Button asChild>
           <Link href={"/"} className="flex items-center gap-2">
             Back to Home Page
