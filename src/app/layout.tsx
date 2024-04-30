2be503dbee6971b7e82df31d9f451a8f21c69999
Sean Gray <seangray.dev@gmail.com>
2024-04-30 18:50:24 -0400

    Update layout metadata and add new OG image generation API route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { CSPostHogProvider } from "@/components/Providers/PostHogClientProvider"
 import SiteFooter from "@/components/layout/navigation/site-footer";
 import SiteNav from "@/components/layout/navigation/site-nav";
 import { Toaster } from "@/components/ui/sonner";
+import siteMetadata from "@/config/site-metadata";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
@@ -14,12 +15,53 @@ const sora = Sora({
 });
 
 export const metadata: Metadata = {
-  title: "Formail",
-  description: "Streamline your workflow with Formail",
+  title: siteMetadata.title,
+  metadataBase: new URL(siteMetadata.siteUrl),
+  description: siteMetadata.description,
+  keywords: [
+    "online forms",
+    "form management",
+    "form submissions",
+    "feedback forms",
+    "contact forms",
+    "survey tool",
+    "data collection",
+    "form builder",
+    "form analytics",
+    "web forms",
+    "email notifications",
+    "spam protection",
+    "secure forms",
+    "custom forms",
+    "form integration",
+    "automated forms",
+    "form processing",
+    "form automation",
+    "form data handling",
+    "form services",
+  ],
+  authors: [{ name: siteMetadata.author }],
+  creator: "GrayTech Solutions",
   openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteMetadata.siteUrl,
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    siteName: siteMetadata.title,
     images: [
       {
-        url: "/api/og",
+        url: "https://formail.dev/api/og",
+        width: 1200,
+        height: 630,
+        alt: "Formail",
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: "https://formail.dev/api/og",
         width: 1200,
         height: 630,
         alt: "Formail",
---
+++ src/config/site-metadata.ts
@@ -0,0 +1,17 @@
+// @ts-check
+
+const siteMetadata = {
+  title: "Formail",
+  author: "GrayTech Solutions",
+  headerTitle: "Formail",
+  description:
+    "Formail offers a streamlined approach to form management, enabling instant notifications and dynamic data handling without pre-structured forms. Ideal for developers, small businesses, and anyone needing efficient form submission management, Formail simplifies the process, making it accessible and user-friendly.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://formail.dev",
+  siteRepo: "https://github.com/seangray-dev/formail-app",
+  siteLogo: "/static/images/logo.png",
+  locale: "en-US",
+};
+
+export default siteMetadata;
