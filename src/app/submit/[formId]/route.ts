165001badd0a656116fefde5a9209688b68eb0dd
Sean Gray <seangray.dev@gmail.com>
2024-05-11 21:57:43 -0400

    refactor route to handle form actions

--- src/app/submit/[formId]/route.ts
+++ src/app/submit/[formId]/route.ts
@@ -9,6 +9,7 @@ import {
   CommentType,
 } from "@cedx/akismet";
 import { ConvexHttpClient } from "convex/browser";
+import { redirect } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
@@ -36,7 +37,7 @@ export async function POST(
 ) {
   const ip = req.headers.get("x-forwarded-for") || req.ip;
   const userAgent = req.headers.get("user-agent") || "";
-  console.log("Rate limiting...");
+
   const { success } = await ratelimit.limit(ip!);
   if (!success) {
     return new NextResponse(JSON.stringify({ error: "Rate limit exceeded" }), {
@@ -46,28 +47,26 @@ export async function POST(
 
   const formId = context.params.formId;
   const form = await convex.query(api.forms.getFormByIdServer, { formId });
+
   if (!form) {
     return new NextResponse(JSON.stringify({ error: "Form not found" }), {
       status: 404,
     });
   }
-  console.log("Determining content type...");
+
   const contentType = req.headers.get("content-type") || "";
   let submissionData: { [key: string]: any } = {};
   let formData: FormData | null = null;
 
   // Decide handling based on content type
   if (contentType.includes("application/json")) {
-    console.log("Handling JSON submission...");
     submissionData = await req.json();
   } else if (contentType.includes("multipart/form-data")) {
-    console.log("Handling multipart/form-data submission...");
     formData = await req.formData();
     submissionData = Object.fromEntries(formData.entries());
   } else if (contentType.includes("application/x-www-form-urlencoded")) {
-    console.log("Handling form-urlencoded submission...");
-    const formData = await req.text();
-    submissionData = Object.fromEntries(new URLSearchParams(formData));
+    const formDataText = await req.text();
+    submissionData = Object.fromEntries(new URLSearchParams(formDataText));
   } else {
     return new NextResponse(
       JSON.stringify({ error: "Unsupported content type" }),
@@ -75,7 +74,6 @@ export async function POST(
     );
   }
 
-  console.log("Checking for spam...");
   const customSpamWords = form.settings.customSpamWords
     ? form.settings.customSpamWords
         .split(",")
@@ -89,49 +87,64 @@ export async function POST(
     customSpamWords,
   );
 
-  console.log("Handling submission...");
-  // Handle file submission separately if multipart/form-data
   if (contentType.includes("multipart/form-data")) {
-    const user = await convex.query(api.users.getUserByFormId, { formId });
-    const filesMetadata = await handleFileUploads(formData!, convex);
-    return await handleFileSubmission(
-      formData!,
-      user,
-      isSpam,
-      formId,
-      filesMetadata,
-    );
+    // Check if any files are present in the FormData
+    let isFilePresent = false;
+    if (!formData) {
+      return;
+    }
+    for (let [key, value] of formData.entries()) {
+      if (value instanceof File) {
+        isFilePresent = true;
+        break;
+      }
+    }
+
+    if (isFilePresent) {
+      const user = await convex.query(api.users.getUserByFormId, { formId });
+      const filesMetadata = await handleFileUploads(formData, convex);
+      await handleFileSubmission(
+        form,
+        formData,
+        user,
+        isSpam,
+        formId,
+        filesMetadata,
+      );
+    } else {
+      // Convert FormData to a normal object for non-file submission
+      submissionData = Object.fromEntries(formData.entries());
+      await handleNonFileSubmission(form, formId, submissionData, isSpam);
+    }
   } else {
-    return await handleNonFileSubmission(formId, submissionData, isSpam);
+    await handleNonFileSubmission(form, formId, submissionData, isSpam);
   }
 }
 
 const handleNonFileSubmission = async (
+  form: any,
   formId: Id<"forms">,
   submissionData: any,
   isSpam: boolean,
 ) => {
-  console.log("Submitting non-file data...");
   try {
-    const result = await convex.mutation(api.submissions.addSubmission, {
+    await convex.mutation(api.submissions.addSubmission, {
       formId,
       data: JSON.stringify(submissionData),
       isSpam,
     });
-    console.log("Submission successful:", result);
-    return new NextResponse(
-      JSON.stringify({ message: "Submission received and processed." }),
-      { status: 200 },
-    );
   } catch (err: any) {
     console.error("Error during submission:", err);
     return new NextResponse(JSON.stringify({ error: err.message }), {
       status: 400,
     });
   }
+  await sendNewSubmissionEmail(form, submissionData);
+  redirect("/submit/success?status=success");
 };
 
 const handleFileSubmission = async (
+  form: any,
   formData: FormData,
   user: any,
   isSpam: boolean,
@@ -173,7 +186,7 @@ const handleFileSubmission = async (
       status: 400,
     });
   }
-
+  await sendNewSubmissionEmail(form, formData);
   return new NextResponse(
     JSON.stringify({ message: "Submission received and email sent!" }),
     { status: 200 },
@@ -217,27 +230,31 @@ const checkForSpam = async (
   return isSpam;
 };
 
-// const sendNewSubmissionEmail = async (form) => {
-//      const { emailRecipients, emailThreads } = form.settings;
-//      const emailRecipientIds: Id<"users">[] = emailRecipients.map(
-//        (id) => id as Id<"users">,
-//      );
+const sendNewSubmissionEmail = async (
+  form: any,
+  submissionData: { [key: string]: any } = {},
+) => {
+  const formId = form._id;
+  const { emailRecipients, emailThreads } = form.settings;
+  const emailRecipientIds: Id<"users">[] = emailRecipients.map(
+    (id: any) => id as Id<"users">,
+  );
 
-//      // testing purposes: flag for skipping emails
-//      const emailActive = process.env.NODE_ENV !== "development";
+  // testing purposes: flag for skipping emails
+  const emailActive = process.env.NODE_ENV !== "development";
 
-//      if (emailRecipientIds.length > 0 && emailActive) {
-//        const recipientEmails = (
-//          await convex.query(api.users.getEmailsForUserIds, {
-//            userIds: emailRecipientIds,
-//          })
-//        ).filter((email): email is string => !!email);
+  if (emailRecipientIds.length > 0 && emailActive) {
+    const recipientEmails = (
+      await convex.query(api.users.getEmailsForUserIds, {
+        userIds: emailRecipientIds,
+      })
+    ).filter((email): email is string => !!email);
 
-//        await convex.action(api.emails.sendNewSubmissionEmail, {
-//          emailThreads,
-//          recipientEmails,
-//          submissionData,
-//          formId,
-//        });
-//      }
-// }
+    await convex.action(api.emails.sendNewSubmissionEmail, {
+      emailThreads,
+      recipientEmails,
+      submissionData,
+      formId,
+    });
+  }
+};
