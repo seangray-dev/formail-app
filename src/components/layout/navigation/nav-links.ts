dc3fcb2b9c9ee8bb168a218aa6e0ee94bb2139ce
Sean Gray <seangray.dev@gmail.com>
2024-05-13 08:13:48 -0400

    refactor site-footer and site-nav to use nav-links

---
+++ src/components/layout/navigation/nav-links.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    title: "Pricing",
+    href: "/pricing",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Docs",
+    href: "https://docs.formail.dev",
+  },
+];
--- src/components/layout/navigation/site-footer.tsx
+++ src/components/layout/navigation/site-footer.tsx
@@ -2,25 +2,48 @@
 
 import Link from "next/link";
 import React from "react";
+import { navLinks } from "./nav-links";
 
 export default function SiteFooter() {
   return (
     <footer className="container items-center border-t py-5">
-      <div className="flex flex-col items-center justify-between gap-2 text-center text-xs text-muted-foreground md:flex-row md:text-left">
-        <div>@{new Date().getFullYear()} Formail. All Rights Reserved.</div>
-        <div className="flex flex-col gap-2 md:flex-row">
-          <Link
-            className="hover:text-white hover:underline"
-            href={"/legal/privacy-policy"}
-          >
-            Privacy Policy
-          </Link>
-          <Link
-            className="hover:text-white hover:underline"
-            href={"/legal/terms-of-service"}
-          >
-            Terms of Service
-          </Link>
+      <div className="flex flex-col-reverse items-center justify-between gap-4 text-center text-xs text-muted-foreground md:flex-row md:text-left">
+        <div>
+          <div>@{new Date().getFullYear()} Formail. All Rights Reserved.</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            {navLinks.map((link, idx) => (
+              <Link
+                key={idx}
+                className="text-muted-foreground hover:text-white hover:underline"
+                href={link.href}
+              >
+                {link.title}
+              </Link>
+            ))}
+            <Link
+              className="hover:text-white hover:underline"
+              target="_blank"
+              href={"https://status.formail.dev"}
+            >
+              Status
+            </Link>
+          </div>
+          <div className="flex justify-center gap-2 md:justify-end">
+            <Link
+              className="hover:text-white hover:underline"
+              href={"/legal/privacy-policy"}
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              className="hover:text-white hover:underline"
+              href={"/legal/terms-of-service"}
+            >
+              Terms of Service
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
